61d6330a77315a46cef650a3e5c1ee4831f32c9d
Meng Le <mzhang@hotmail.co.uk>
2019-02-11 11:56:48 +0000

    Deleted a not used centroid script (checked it was a copy paste from another project).
    So ran the routine to get nearest centre and main centre. Now the distance from centre file is okay

--- Data generation/Urban centres/Generating distance from centres variable.R
+++ Data generation/Urban centres/Generating distance from centres variable.R
@@ -5,73 +5,52 @@
 
 library(RANN)
 
+# 1) Loading in PWC and ttwa lkp -----
+# A look up file for datazones and lsoas to ttwa11 is already created in lookup
+##  table ...R So just load it in
+zone2ttwa.lkp <-
+  read.csv('Saved generated data/lsoas and datazones to ttwa lkp.csv')
 
-# 1) Load in pop weighted centroids ---------------------------------------
-lsoa01.sf <- st_read(
-  dsn = google.drive.spatial %>%
-    paste('/LSOA 2001', sep = ''),
-  layer = 'Lower_Layer_Super_Output_Areas_December_2001_Population_Weighted_Centroids') %>%
-  st_transform(crs = st_crs(ukgrid))
-
-lsoa01.coords <- data.frame(lsoa01 = lsoa01.sf$lsoa01cd,
-                          lsoa01.sf %>% st_coordinates)
-##  merge with TTWA2011 info
-lsoa01tottwa11.lkp <-
-  read.csv('Working analysis files/lsoa01 to ttwa11 lkp.csv')
-
-##  Not perfect lsoa01 to ttwa11
-lsoa01.coords <- lsoa01.coords %>% left_join(lsoa01tottwa11.lkp)
-
+##  coords are X and Y
 
 # 2) Load in the centres data ---------------------------------------------
 centres.imputed <-
-  read.csv('Working analysis files/Imputed centres based on osmdata.csv')
+  read.csv('Saved generated data/Imputed centres based on osmdata for England and Scotland.csv')
 
-##  Find which TTWA they belong to
+##  Find which TTWA they belong to-- so convert to sf and load in ttwa11
 centre.sf <- centres.imputed %>% 
   st_as_sf(coords = c('imputed_easting', 'imputed_northing'),
            crs = st_crs(ukgrid))
+
 ttwa11.sf <- st_read(dsn = google.drive.spatial %>% 
   paste('/TTWA 2011', sep = ''),
   layer = 'Travel_to_Work_Areas_December_2011_Full_Extent_Boundaries_in_United_Kingdom') %>%
   st_transform(crs = st_crs(ukgrid))
 
 ##  Find which centres belong inside with TTWA
-centre.sf <- centre.sf %>% st_join(ttwa11.sf)
+centre.sf <- 
+  centre.sf %>% st_join(ttwa11.sf)
 centre.sf %>% summary
-centre.sf$ttwa11cd %>% is.na %>% table
-##  Add to the centre.imputed data
-centres.imputed <- centres.imputed %>%
+centre.sf %>% filter(ttwa11cd %>% is.na)# 4 centres without ttwa
+## very rural though so fine
+
+##  Add new vars to the non-sf file
+centres.imputed <- 
+  centres.imputed %>%
   mutate(ttwa11cd = centre.sf$ttwa11cd,
          ttwa11nm = centre.sf$ttwa11nm)
+centre.sf
 
-##  Any over 10k and 30 k respectively
-centres10k.imputed <- centres.imputed %>% filter(pop11 > 10000)
-centres30k.imputed <- centres.imputed %>% filter(pop11 > 30000)
-
-
-# 3) Time to find the nearest centre over 30k------------------------------
-nn.list <- nn2(query = lsoa01.coords %>% dplyr::select(X, Y) %>% as.matrix,
-    data = centres30k.imputed %>% dplyr::select(imputed_easting, imputed_northing) %>% as.matrix,
-    k = 1)
-
-nearest30k.lkp <- data.frame(lsoa01cd = lsoa01.coords$lsoa01,
-                             nearest_dist30k = nn.list$nn.dists %>% c,
-                             nearest_bua30k = centres30k.imputed$name[nn.list$nn.idx])
-nearest30k.lkp %>% 
-  write.csv('Working analysis files/Distance from nearest centre for LSOA01 (30k).csv',
-            row.names = F)
-
-
-# 4) Finding the nearest significant centre -------------------------------
+# 3) Finding the nearest significant centre -------------------------------
 ##  Critera is to include an centre over 10k but also half the the largest centre
 ##  for the TTWA. Also deffo include over 60k
 ##  Note: For reasons our ttwa lkp and distances files is combined here
 
+zone2ttwa.lkp$ttwa11nm %>% is.na %>% sum ## okay np
 ttwa.nms <- 
-  lsoa01.coords$TTWA11NM %>% unique
-lsoa01.coords$TTWA11NM %>% table
-
+  zone2ttwa.lkp$ttwa11nm %>%
+  na.omit %>%
+  unique
 
 nearest_paper.list <- list(NULL) 
 
@@ -79,16 +58,20 @@ nearest_paper.list <- list(NULL)
 for (i in 1:length(ttwa.nms)){
   temp.ttwa <- ttwa.nms[i]
   print(temp.ttwa)
-  
+
   ##  Now select the centre in the ttwa and find the largest -- this creates a 
   ##  variable called cutoff which we will use to calculat distance
-  large.cent <- centres10k.imputed %>% 
+  ##  So filter to over 10k first and isolate to ttwa
+  large.cent <- centres.imputed %>% 
+    filter(pop11 > 10000) %>%
     filter(ttwa11nm == temp.ttwa) %>%
     arrange(- pop11) #important to arrnge by pop size
   
-  temp.cut <- 10000 # set default cut off
+  ## Set cut off if there is a centre to half largest bua
   
-  ## set cut off if there is a centre to half largest bua
+  temp.cut <- 10000 # set default cut off for ttwa pop
+  
+  
   if(large.cent %>% nrow >= 1){
     temp.cut <- large.cent$pop11[1] / 2
   }
@@ -97,13 +80,13 @@ for (i in 1:length(ttwa.nms)){
   if(temp.cut > 60000){temp.cut <- 60000}
   
   ##  Now to take the nearest distance
-  temp.lsoa <- lsoa01.coords %>% 
-    filter(TTWA11NM == temp.ttwa)
+  temp.lsoa <- zone2ttwa.lkp %>% 
+    filter(ttwa11nm == temp.ttwa)
   
   
-  temp.centre <- centres10k.imputed %>% filter(pop11 >= temp.cut)
-  temp.main <- large.cent[1, ]
-  
+  temp.centre <- centres.imputed %>% filter(pop11 >= temp.cut) ## filter to ALL ttwas over pop cut off
+  temp.main <- large.cent[1, ] # selecting largest bua/loc within ttwa by pop centre
+
   ##  Now to run the nearest neightbour search separately ----
   ##  For the main distance -- we need an if else statement in case zeroes
   if(large.cent %>% nrow >= 1){
@@ -119,7 +102,7 @@ for (i in 1:length(ttwa.nms)){
              main_bua = NA)
     
   )
-  
+
   ##  nearest dist
   nn_nearest <- nn2(query = temp.lsoa %>% dplyr::select(X, Y) %>% as.matrix,
                    data = temp.centre %>% dplyr::select(imputed_easting, imputed_northing) %>% as.matrix,
@@ -132,12 +115,12 @@ for (i in 1:length(ttwa.nms)){
   nearest_paper.list[[i]] <- temp.lsoa
 }
 
-##  Saving the nearest distance data from section 4 ----
+##  Saving the nearest distance data from section 3 ----
 nearest_paper.lkp <-
   do.call(rbind, nearest_paper.list)
 
 
 nearest_paper.lkp %>% 
   dplyr::select(-X, -Y) %>%
-  write.csv('Working analysis files/Distance from nearest centre for LSOA01 and TTWA lkp (paper).csv',
+  write.csv('Saved generated data/Distance from nearest centre for zones and TTWA lkp.csv',
             row.names = F)
