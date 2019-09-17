c1b3960b3c1ac2f26526efa696b3bdbf639591c2
Meng Le <mzhang@hotmail.co.uk>
2019-09-17 14:49:50 +0100

    Changes for holding amenities and etc constant.
    1) Deleted the old results by LA
    2) So created DCI results for all years except 2015 and 2016 because those years use 2011 LSOA deinfitions (See next)
    3) Then because the rest use 2001 LSOA, we can simply change the value of every year's crime etc with their 2004 value
    4) ... except for crime in Scotland where we use 2006 value since crime wasnt in IMD2004.
    5) Work and live.in isn't actually changed at all since they only use 2001 data and are unchanged over this period.
    6) So only affects crime, geo and pm25

--- Analysis and paper outputs/Producing the Duncan index.R
+++ Analysis and paper outputs/Producing the Duncan index.R
@@ -1,6 +1,6 @@
 ##  Producing the Duncan index 
 ##  We need to basically load in the data from tables to produce the duncan index
-##  for every ttwa and/or la 
+##  for every ttwa using amenities values that 1) Change over time and 2) are fixed over time
 
 ##  Step 1) Now we need to get all the data that we need from various files
 eng04 <- read.csv('Saved generated data/Formatted English data 2004.csv') 
@@ -16,6 +16,24 @@ sco16 <- read.csv('Saved generated data/Formatted Scotland data 2016.csv')
 combined.tab <- rbind(eng04, eng07, eng10, eng15,
                       sco04, sco06, sco09, sco12, sco16)
 
+
+##  Creating the stats fixed at 2004 quantities; we do not fill in anything if imd == 2015 or 2016
+combined.tab <-
+  combined.tab %>%
+  group_by(zone) %>%
+  mutate(
+    crime_fixed = ifelse(country == 'England' & year != 2015, 
+                         crime[year == '2004'], 
+                         ifelse(country == 'Scotland' & year != 2016,
+                                      crime[year == '2006'], 
+                         NA)
+                         ),# Since scotland missing crime at 2004
+    geo_fixed = ifelse(year %in% c(2015, 2016), NA, geo[year == '2004']),
+    pm25_fixed = ifelse(year %in% c(2015, 2016), NA, pm25[year == '2004'])
+  ) %>%
+  ungroup
+
+
 ##  Save a copy of the combined table: this is used for other linkage operations
 combined.tab %>% 
   write.csv('Saved generated data/Master formated data of variables for England and Scotland (UI paper1).csv',
@@ -23,8 +41,6 @@ combined.tab %>%
 
 ##  Step 2) Caculate the duncan index for every combination of year and ttwa / la
 ##  TTWA
-combined.tab %>% str
-
 ttwa.tab <- 
   combined.tab %>%
   group_by(year, ttwa, country) %>% ## we want to calculate stats by year, ttwa and country
@@ -40,32 +56,16 @@ ttwa.tab <-
             geo = dindex(x = noninc.n, y = inc.n, sort.var = geo),
             work = dindex(x = noninc.n, y = inc.n, sort.var = work),
             pm25 = dindex(x = noninc.n, y = inc.n, sort.var = pm25),
-            suburb = dindex(x = noninc.n, y = inc.n, sort.var = -1 * pop / area)
+            suburb = dindex(x = noninc.n, y = inc.n, sort.var = -1 * pop / area),
+  ##  Fixed Dindex            
+            crime_fixed = dindex(x = noninc.n, y = inc.n, sort.var = crime_fixed),
+            geo_fixed = dindex(x = noninc.n, y = inc.n, sort.var = geo_fixed),
+            pm25_fixed = dindex(x = noninc.n, y = inc.n, sort.var = pm25_fixed)
+            
   )
-
 
 ttwa.tab %>% write.csv('Results/Duncan index by TTWA.csv')
-cor(ttwa.tab[, 4:12], use= "pairwise.complete.obs", method = 'spearman')
+cor(ttwa.tab[, 4:19], use= "pairwise.complete.obs", method = 'spearman')
+## Large correlation between ranks for crime etc
 
-##  LA
-la.tab <- combined.tab %>%
-  group_by(year, la, country) %>% ## we want to calculate stats by year, ttwa and country
-  summarise(total.pop = sum(pop), #name of summary statistic and how its calculated; i.e total.pop is sum of pop 
-            total.area = sum(area),
-            prop.inc = sum(inc.n) / sum(pop),
-            dist_main = dindex(x = noninc.n, y = inc.n, sort.var = dist_main), #name of summary statistic and how its calculated
-            dist_nearest = dindex(x = noninc.n, y = inc.n, sort.var = dist_nearest),
-            di = sum( abs((inc.n / sum(inc.n)) - (noninc.n / sum(noninc.n))) ) / 2,
-            concen = sum( abs((inc.n / sum(inc.n)) - (area / sum(area))) ) / 2,
-            live.in = dindex(x = noninc.n, y = inc.n, sort.var = live.in),
-            crime = dindex(x = noninc.n, y = inc.n, sort.var = crime),
-            geo = dindex(x = noninc.n, y = inc.n, sort.var = geo),
-            work = dindex(x = noninc.n, y = inc.n, sort.var = work),
-            pm25 = dindex(x = noninc.n, y = inc.n, sort.var = pm25),
-            suburb = dindex(x = noninc.n, y = inc.n, sort.var =  -1 * pop / area)
-  )
-
-la.tab
-la.tab %>% write.csv('./Results/Duncan index by LA.csv')
-cor(la.tab[, 4:12], use= "pairwise.complete.obs", method = 'spearman')
-##  End 
+##  End
